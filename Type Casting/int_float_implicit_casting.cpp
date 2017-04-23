c833f383597b6864499cb387b44d8a529251054f
Santosh Kumar Tandrothu <Santosh8533@users.noreply.github.com>
2017-04-23 15:11:49 -0700

    Add files via upload

--- Type Casting/int_float_implicit_casting.cpp
+++ Type Casting/int_float_implicit_casting.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main(){
     int i=4;
-    float j=5.1;
-    cout<<"Hello World";
+    double j=5.1; //if j is 5.0, the .0 is ignored
     auto x=i- j;  //auto should be always initialized
-    cout<<"Result:"<<x<<endl;
+    cout<<"J="<<j<<endl;
+    cout<<"Result:"<<x<<"size:"<<sizeof(x)<<endl;
 return 0;
 }
 
@@ -17,3 +17,7 @@ else the result x is float, auto becomes float.
     i=5 j=5.1 The result is -0.1. So auto becomes float.
 Note: int is unsigned by default. To make it signed, add signed keyword before it.
     */
+/*Type conversion rules:
+    1) If eithe operand is double, the other operand becomes double
+    2) If either operand is float, the other operand becomes float.
+    http://stackoverflow.com/questions/5563000/implicit-type-conversion-rules-in-c-operators*/
