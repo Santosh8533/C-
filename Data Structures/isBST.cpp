94c257fb63e8b56f0998f3d34382ecb818ab8d84
Santosh Kumar Tandrothu <Santosh8533@users.noreply.github.com>
2018-02-03 12:47:35 -0800

    Fixed a bug

--- Data Structures/isBST.cpp
+++ Data Structures/isBST.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<limits>
 
 using namespace std;
 
@@ -21,6 +22,7 @@ class BinaryTree{
     void displayTree();
     void inOrderTraversal(Node*);
     bool isBST();
+    bool isBST(Node*, int,int);
 
 };
 
@@ -105,6 +107,42 @@ void BinaryTree::inOrderTraversal(Node* currentRoot){
     inOrderTraversal(currentRoot->right);
 }
 
+/** Is it a BST?**/
+/*
+bool BinaryTree::isBST(){
+    if(root==nullptr){return false;}
+    int leftMax =0, rightMax = 0;
+    cout << "Left subtree" << endl;
+    isBST(root->left, &leftMax);
+    cout << "Right subtree" << endl;
+    isBST(root->right, &rightMax);
+    if(leftMax < root->value && rightMax > root->value){return true;}
+    return false;
+}
+
+void BinaryTree::isBST(Node* currentRoot, int* Max){
+    if(currentRoot == nullptr){return;}
+    if(*Max < currentRoot->value){
+            *Max = currentRoot->value;
+            cout << *Max << "\t";
+    }
+    isBST(currentRoot->left, Max);
+    isBST(currentRoot->right, Max);
+    return;
+}*/
+
+bool BinaryTree::isBST(){
+    return isBST(root,INT_MIN,INT_MAX );
+}
+
+bool BinaryTree::isBST(Node* currentRoot, int MIN, int MAX){
+
+    if(currentRoot==nullptr){return true;}
+    if(currentRoot->value > MIN && currentRoot->value < MAX){return true;}
+    else return false;
+    return isBST(currentRoot->left, MIN, currentRoot->value-1) &&
+           isBST(currentRoot->right, currentRoot->value+1, MAX);
+}
 
 int main(){
 
@@ -114,7 +152,10 @@ int main(){
     bt.insertNode(100);
     bt.insertNode(2);
     bt.insertNode(45);
+    bt.insertNode(120);
+    bt.insertNode(80);
     bt.displayTree();
+    cout << "Is this tree a BST?" << bt.isBST()<<endl;
 
     return 0;
 }
