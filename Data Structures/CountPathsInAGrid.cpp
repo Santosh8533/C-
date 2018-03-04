4f3821ce3897cbc9d2d86a7631e6d5965f635783
Santosh Kumar Tandrothu <Santosh8533@users.noreply.github.com>
2018-03-03 19:33:21 -0800

    Update CountPathsInAGrid.cpp

--- Data Structures/CountPathsInAGrid.cpp
+++ Data Structures/CountPathsInAGrid.cpp
@@ -4,7 +4,8 @@
 
 using namespace std;
 
-int countPaths(int grid[2][2], int rowSize,int columnSize,int row, int column){
+/********************************************************************************/
+int countPaths(int grid[3][3], int rowSize,int columnSize,int row, int column){
 
     if(row == rowSize-1 || column == columnSize-1 ){return 1;}
     int totalPaths =0;
@@ -13,20 +14,18 @@ int countPaths(int grid[2][2], int rowSize,int columnSize,int row, int column){
 
     return totalPaths;
 }
+/********************************************************************************/
 
-int countPaths(int grid[2][2],int rowSize, int columnSize){
-    /*cout << "size of array" << sizeof(a) << endl;
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            cout << a[i][j] << "\t";
-        }
-    }*/
+int countPaths(int grid[3][3],int rowSize, int columnSize){
+    
     int noOfPaths = countPaths(grid,rowSize,columnSize,0,0);
     return noOfPaths;
+    
 }
+/********************************************************************************/
 int main(){
-    //int grid[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
-    int grid[2][2];
+    int grid[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    //int grid[2][2];
     int rowSize = sizeof(grid)/sizeof(grid[0]);
     int columnSize = sizeof(grid[0])/sizeof(int);
     //cout << rowSize << "\t" << columnSize << endl;
