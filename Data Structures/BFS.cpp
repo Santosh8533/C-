9316ff1f597ab41625b11f938b83eca28fc94305
Santosh Kumar Tandrothu <Santosh8533@users.noreply.github.com>
2018-05-05 12:07:20 -0700

    Update BFS.cpp

--- Data Structures/BFS.cpp
+++ Data Structures/BFS.cpp
@@ -1,95 +1,89 @@
-// Program to print BFS traversal from a given
-// source vertex. BFS(int s) traverses vertices 
-// reachable from s.
 #include<iostream>
-#include <list>
- 
+#include<list>
+#include<queue>
+
 using namespace std;
- 
-// This class represents a directed graph using
-// adjacency list representation
-class Graph
-{
-    int V;    // No. of vertices
- 
-    // Pointer to an array containing adjacency
-    // lists
-    list<int> *adj;   
+
+class Graph{
+    int v;
+    list<int> *adj;
 public:
-    Graph(int V);  // Constructor
- 
-    // function to add an edge to graph
-    void addEdge(int v, int w); 
- 
-    // prints BFS traversal from a given source s
-    void BFS(int s);  
+    Graph(int);
+    void addEdge(int,int);
+    void printAdjList();
+    void BFS(int);
 };
- 
-Graph::Graph(int V)
-{
-    this->V = V;
-    adj = new list<int>[V];
+
+Graph::Graph(int v){
+    this->v = v;
+    adj = new list<int>[v];
 }
- 
-void Graph::addEdge(int v, int w)
-{
-    adj[v].push_back(w); // Add w to v’s list.
+
+void Graph::addEdge(int souVer, int DestVer){
+    adj[souVer].push_back(DestVer);
 }
- 
-void Graph::BFS(int s)
-{
-    // Mark all the vertices as not visited
-    bool *visited = new bool[V];
-    for(int i = 0; i < V; i++)
+
+void Graph::printAdjList(){
+    list<int>::iterator it;
+    for(int i=0;i<v;i++){
+            cout << i<<"-->";
+        for(it=adj[i].begin();it!=adj[i].end();++it){
+            cout << *it<<"-->";
+        }
+        cout <<endl;
+    }
+}
+void Graph::BFS(int ver){
+
+    //create an array to keep track of visited nodes
+    bool *visited = new bool[v];
+    for(int i=0;i<v;i++){
         visited[i] = false;
- 
-    // Create a queue for BFS
-    list<int> queue;
- 
-    // Mark the current node as visited and enqueue it
-    visited[s] = true;
-    queue.push_back(s);
- 
-    // 'i' will be used to get all adjacent
-    // vertices of a vertex
-    list<int>::iterator i;
- 
-    while(!queue.empty())
-    {
-        // Dequeue a vertex from queue and print it
-        s = queue.front();
-        cout << s << " ";
-        queue.pop_front();
- 
-        // Get all adjacent vertices of the dequeued
-        // vertex s. If a adjacent has not been visited, 
-        // then mark it visited and enqueue it
-        for (i = adj[s].begin(); i != adj[s].end(); ++i)
-        {
-            if (!visited[*i])
-            {
-                visited[*i] = true;
-                queue.push_back(*i);
+    }
+
+    //Create a queue to save the vertices
+    queue<int> que;
+
+    visited[ver] = true;
+    que.push(ver);
+
+    list<int>::iterator it;
+
+    while(!que.empty()){
+        ver = que.front();
+        cout<<ver<<endl;
+        que.pop();
+
+        for(it=adj[ver].begin();it!=adj[ver].end();++it){
+            if(!visited[*it]){
+                visited[*it] = true;
+                que.push(*it);
             }
         }
     }
 }
- 
-// Driver program to test methods of graph class
-int main()
-{
-    // Create a graph given in the above diagram
-    Graph g(4);
-    g.addEdge(0, 1);
-    g.addEdge(0, 2);
-    g.addEdge(1, 2);
-    g.addEdge(2, 0);
-    g.addEdge(2, 3);
-    g.addEdge(3, 3);
- 
-    cout << "Following is Breadth First Traversal "
-         << "(starting from vertex 2) \n";
-    g.BFS(2);
- 
+int main(){
+    Graph g(7);
+
+    g.addEdge(1,2);
+    g.addEdge(2,1);
+    g.addEdge(1,3);
+    g.addEdge(3,1);
+    g.addEdge(2,4);
+    g.addEdge(4,2);
+    g.addEdge(2,5);
+    g.addEdge(5,2);
+    g.addEdge(3,5);
+    g.addEdge(5,3);
+    g.addEdge(4,5);
+    g.addEdge(5,4);
+    g.addEdge(4,6);
+    g.addEdge(6,4);
+    g.addEdge(5,6);
+    g.addEdge(6,5);
+
+    g.printAdjList();
+    g.BFS(3);
+
     return 0;
 }
