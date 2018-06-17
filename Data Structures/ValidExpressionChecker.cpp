2c0013f089f15b76422e695d29f639aa1e7dd135
Santosh Kumar Tandrothu <Santosh8533@users.noreply.github.com>
2018-06-17 12:07:02 -0700

    Create ValidExpressionChecker.cpp

---
+++ Data Structures/ValidExpressionChecker.cpp
@@ -0,0 +1,70 @@
+    /**
+        Author: Santosh Tandrothu
+        Date: 06/17/2018
+        Problem: Given an mathematical expression, check if has balanced parantheses
+        Example: (1+2)*3, }{1+2*(3+4)}
+        Answer:  (1+2)*3= Valid, }{1+2*(3+4)} = Not Valid
+    **/
+
+    /***
+        Time Complexity = O(n)
+        Space Complexity = O(stack size)
+    **/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool hasMatchingParantheses(string strExpression) {
+    stack<char> s;
+    char c;
+    int len = strExpression.length();
+    for(int i=0;i<len;i++){
+        //To catch closing braces in the beginning Ex: }(1+2)*3
+        if((strExpression[i]==')' || strExpression[i]=='}' || strExpression[i]==']') && s.empty()){return false;}
+
+        else if(strExpression[i]=='(' || strExpression[i]=='{' || strExpression[i]=='['){
+            s.push(strExpression[i]);
+        }
+
+        else if((strExpression[i]==')' || strExpression[i]=='}' || strExpression[i]==']') && !s.empty()){
+            switch(strExpression[i]){
+                case ')':
+                    c = s.top();
+                    s.pop();
+                    if(c != '('){return false;}
+                    break;
+
+                case '}':
+                    c = s.top();
+                    s.pop();
+                    if(c != '{'){return false;}
+                    break;
+
+                case ']':
+                    c = s.top();
+                    s.pop();
+                    if(c != '['){return false;}
+                    break;
+            }
+        }
+    }
+    return true;
+
+}
+
+
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    bool res;
+    string strExpression;
+    getline(cin, strExpression);
+
+    res = hasMatchingParantheses(strExpression);
+    fout << res << endl;
+
+    fout.close();
+    return 0;
+}
