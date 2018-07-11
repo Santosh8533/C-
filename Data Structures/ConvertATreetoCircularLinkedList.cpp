a93e17bec39345f027f2105c5dc8fa3010a5b607
Santosh Kumar Tandrothu <Santosh8533@users.noreply.github.com>
2018-07-10 23:14:36 -0700

    Optimized the code

--- Data Structures/ConvertATreetoCircularLinkedList.cpp
+++ Data Structures/ConvertATreetoCircularLinkedList.cpp
@@ -1,98 +1,210 @@
-// C++ program to convert a given Binary
-// Tree to Doubly Linked List
-#include <stdio.h>
-#include <stdlib.h>
+        /**
+            Author: Santosh Tandrothu
+            Date: 07/10/2018
+            Problem: Convert Tree to Circular Doubly Linked List
 
-// Structure for tree and linked list
-struct Node
-{
-	int data;
-	Node *left, *right;
+            Algorithm:
+
+        **/
+
+        /***
+            Time Complexity = O(n) (Traversing all the nodes in a tree)
+            Space Complexity = O(h) (height of the tree)
+        **/
+
+#include<iostream>
+#include<limits>
+
+using namespace std;
+
+class Node{
+    public:
+    int value;
+    Node *left;
+    Node *right;
 };
 
-void BToDLL(Node* root, Node** head_ref)
-{
-	// Base cases
-	if (root == NULL)
-		return;
+class BinaryTree{
 
-	// Recursively convert right subtree
-	BToDLL(root->right, head_ref);
+    Node *root,*head,*tail;
 
-	// insert root into DLL
-	if(!root->right)root->right = *head_ref;
+    public:
+    BinaryTree();
+    Node* createNode(int);
+    void insertNode(int);
+    void insertNode(Node*,Node*);
+    void displayTree();
+    void inOrderTraversal(Node*);
+    bool isBST();
+    bool isBST(Node*, int,int);
+    Node* BSTtoLL();
+    Node* BSTtoLL(Node*, Node**);
+    void displayList();
 
-	// Change left pointer of previous head
-	if (*head_ref != NULL)
-		(*head_ref)->left = root;
+};
 
-	// Change head of Doubly linked list
-	*head_ref = root;
-
-	// Recursively convert left subtree
-	BToDLL(root->left, head_ref);
+/** Constructor**/
+BinaryTree::BinaryTree(){
+    root = nullptr;
 }
 
-//Function to convert doubly list to circular
+/** Create a node**/
+Node* BinaryTree::createNode(int val){
+    //Assign memory
 
-void DoublyToCircular(Node* head){
-    Node* tail = nullptr;
-    tail = head;
-    while(tail->right!=nullptr){
-        tail = tail->right;
+    Node *node = new Node;
+
+    //Initialize
+    node->value = val;
+    node->left = nullptr;
+    node->right = nullptr;
+
+    return node;
+}
+
+/** Insert a node**/
+void BinaryTree::insertNode(int val){
+
+    Node* node = createNode(val);
+    //If tree is empty
+    if(root == nullptr){
+        root = node;
+        //cout<<"root value" <<root->value<<endl;
+        return;
     }
-    head->left = tail;
-    tail->right = head;
+
+    insertNode(root,node);
+
     return;
 }
-// Utility function for allocating node for Binary
-// Tree.
-Node* newNode(int data)
-{
-	Node* node = new Node;
-	node->data = data;
-	node->left = node->right = NULL;
-	return node;
+
+/** Insert a node - function overwriting**/
+void BinaryTree::insertNode(Node* curRoot, Node *node){
+
+    if(curRoot == nullptr){
+        return;
+    }
+
+    if(node->value < curRoot->value){
+        insertNode(curRoot->left, node);
+        if(curRoot->left == nullptr){
+                curRoot->left = node;
+                //cout << "Inserted at left->" << curRoot->left->value <<endl;
+        }
+    }
+
+    else if(node->value > curRoot->value){
+        insertNode(curRoot->right, node);
+        if(curRoot->right == nullptr){
+                curRoot->right = node;
+               // cout << "Inserted at right->" << curRoot->right->value <<endl;
+        }
+    }
+
+    return;
 }
 
-// Utility function for printing double linked list.
-void printList(Node* head)
-{
-	printf("Extracted Double Linked list is:\n");
-	while (head)
-	{
-		printf("%d ", head->data);
-		head = head->right;
-		//printf("%d ", head->data);
-	}
+/** Display Tree - inorder traversal**/
+void BinaryTree::displayTree(){
+    inOrderTraversal(root);
+    return;
+
 }
 
-// Driver program to test above function
-int main()
-{
-	/* Constructing below tree
-			5
-			/ \
-			3	 6
-		/ \	 \
-		1 4	 8
-		/ \	 / \
-		0 2	 7 9 */
-	Node* root = newNode(5);
-	root->left = newNode(3);
-	root->right = newNode(6);
-	root->left->left = newNode(1);
-	root->left->right = newNode(4);
-	root->right->right = newNode(8);
-	root->left->left->left = newNode(0);
-	root->left->left->right = newNode(2);
-	root->right->right->left = newNode(7);
-	root->right->right->right = newNode(9);
+/** Inorder traversal**/
+void BinaryTree::inOrderTraversal(Node* currentRoot){
 
-	Node* head = NULL;
-	BToDLL(root, &head);
+    if(currentRoot==nullptr)return;
 
-	printList(head);
+    inOrderTraversal(currentRoot->left);
+    cout<<currentRoot->value<<"\t"<<endl;
+    inOrderTraversal(currentRoot->right);
+}
 
-	return 0;
+
+bool BinaryTree::isBST(){
+    return isBST(root,INT_MIN,INT_MAX );
+}
+
+bool BinaryTree::isBST(Node* currentRoot, int MIN, int MAX){
+
+//IF it reaches end of the tree, that means the nodes along this subtree met the
+//criteria, so true can be returned - if doesn't 'else' loop takes care of it
+    if(currentRoot==nullptr){return true;}
+    if(currentRoot->value > MIN && currentRoot->value < MAX){
+            return isBST(currentRoot->left, MIN, currentRoot->value)&&
+                   isBST(currentRoot->right, currentRoot->value, MAX);
+    }
+    else return false;
+}
+
+/** Convert BST to LL Utility function**/
+
+Node* BinaryTree::BSTtoLL(Node* curr_root, Node** prev){
+
+    if(curr_root == nullptr){return nullptr;}
+    //Traverse left
+    BSTtoLL(curr_root->left,prev);
+    //if it is leftmost/leaf node,save it as head
+    if(*prev == nullptr){head = curr_root;}
+    //if it is left node(not leaf) or root, establish prev and next connections
+    else{
+        //prev
+        curr_root->left = *prev; //1<-2
+        //next
+        (*prev)->right = curr_root; //1->2
+    }
+    //update prev to curr_root as the current visited node becomes previous node to
+    //next node
+    *prev = curr_root;
+    //Traverse right
+    BSTtoLL(curr_root->right,prev);
+    //if(tail==nullptr){tail = curr_root;}
+
+    return head;
+
+}
+
+/** Convert BST to LL**/
+Node* BinaryTree::BSTtoLL() {
+    if(root == nullptr) return nullptr;
+    Node* prev = nullptr;
+    Node* head = BSTtoLL(root,&prev);
+    return head;
+}
+
+/**Display LL**/
+void BinaryTree::displayList(){
+    if(head == nullptr){return;}
+    Node* temp = head;
+    while(temp!=nullptr){
+        cout<<temp->value<<"\t";
+        temp = temp->right;
+    }
+    cout<<endl;
+
+    /*if(tail == nullptr){return;}
+    temp = tail;
+    while(temp!=nullptr){
+        cout<<temp->value<<"\t";
+        temp = temp->left;
+    }
+    cout<<endl;*/
+}
+
+int main(){
+
+    BinaryTree bt;
+    bt.insertNode(4);
+    bt.insertNode(2);
+    bt.insertNode(6);
+    bt.insertNode(2);
+    bt.insertNode(1);
+    bt.insertNode(3);
+    bt.insertNode(5);
+    bt.insertNode(7);
+    bt.displayTree();
+    Node* list_head= bt.BSTtoLL();
+    bt.displayList();
+    return 0;
 }
