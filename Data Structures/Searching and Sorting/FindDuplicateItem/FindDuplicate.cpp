3b3efb83ddeaddf846ace3ad362fa423ceb318c0
Santosh Kumar Tandrothu <13855999+SantoshKumarTandrothu@users.noreply.github.com>
2018-12-24 02:34:19 -0600

    Find Duplicate Item

--- Data Structures/Searching and Sorting/FindDuplicateItem/FindDuplicate.cpp
+++ Data Structures/Searching and Sorting/FindDuplicateItem/FindDuplicate.cpp
@@ -1,1 +1,45 @@
 #include<iostream>
+
+using namespace std;
+
+int findRepeat(const vector<int>& inputNumbers){
+	int start_ = 0;
+	int end_ = inputNumbers().size()-1;	
+	
+	while(start_ < end_){
+		int mid = start_ + (end_-start_)/2;
+		
+		int lowerRangeStart = start_;
+		int lowerRangeEnd = mid;
+	
+		int upperRangeStart = mid+1;
+		int upperRangeEnd = end_;
+		
+		int itemsInlowerRange = 0;
+		
+		for(int i:inputNumbers){
+			if(i>=lowerRangeStart && i<=lowerRangeEnd){
+				itemsInlowerRange++;
+			}
+		
+			int idealItemsInLowerRange = mid+1;
+			
+			if(itemsInlowerRange > idealItemsInLowerRange){
+				start_ = lowerRangeStart
+				end_ = lowerRangeEnd;
+			}
+			else {
+				start_ = upperRangeStart;
+				end_ = upperRangeEnd;
+			}
+		}
+	}
+	return start_;
+}
+
+int main(){
+	
+	vector<int> inputNumbers = {1,2,3,4,5,6,7,8,9,9,10};
+	cout<<"The repeated number is:"<<findRepeat(inputNumbers);
+	return 0;
+}
