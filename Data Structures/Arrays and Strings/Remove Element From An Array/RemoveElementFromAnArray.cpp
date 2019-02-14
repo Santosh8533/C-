145e25f73b5d00008f65cb7174fd97d59b9bdd7d
Santosh Kumar Tandrothu <13855999+SantoshKumarTandrothu@users.noreply.github.com>
2019-02-14 08:16:27 -0800

    Add files via upload

--- Data Structures/Arrays and Strings/Remove Element From An Array/RemoveElementFromAnArray.cpp
+++ Data Structures/Arrays and Strings/Remove Element From An Array/RemoveElementFromAnArray.cpp
@@ -1,30 +1,26 @@
-int Solution::removeElement(vector<int> &A, int B) {
-    sort(A.begin(),A.end()); //111243
-    int start=-1,end=0;
-    queue<int> q;
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int removeElement(vector<int> &A, int B) {
+    int count_=0;
     for(int i=0;i<A.size();i++){
         if(A[i]==B){
-            if(start==0)start = i; //0
-            else end=start+1; //2
+            continue;
         }
-        else if(start>=0){
-            q.push(A[i]);
-        }
+        A[count_]=A[i];
+        count_++;
     }
-    int len = A.size()-1;
-    int count = len-end; //
-    while(count>0){
-        A.pop_back();
-        count--;
+    return count_;
+}
+
+
+int main(){
+    vector<int> v ={2,1,4,1,1};
+    int res = removeElement(v,1);
+    for(int i=0;i<res;i++){
+        cout<<v[i]<<endl;
     }
 
-    for(int i=end;i>=start;i--){
-        A.pop_back();
-    }
-
-    while(!q.empty()){
-        A.push_back(q.front());
-        q.pop();
-    }
-    return A.size();
 }
